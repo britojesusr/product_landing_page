e199e11f290ea89eed3e5ea44c27ffdc25c32059
britojesusr <britojesus931@gmail.com>
2020-07-29 22:53:51 -0400

    efecto cambiar colores a los telefonos al presionar botones

---
+++ interactividad.js
@@ -0,0 +1,60 @@
+function a8black() {
+   document.getElementById('a8black').style.display="flex";
+   document.getElementById('a8blue').style.display="none";
+   document.getElementById('a8orange').style.display="none";
+}
+
+function a8blue() {
+    document.getElementById('a8black').style.display="none";
+    document.getElementById('a8blue').style.display="flex";
+    document.getElementById('a8orange').style.display="none";
+ }
+
+ function a8orange() {
+    document.getElementById('a8black').style.display="none";
+    document.getElementById('a8blue').style.display="none";
+    document.getElementById('a8orange').style.display="flex";
+ }
+ 
+
+ function note8black() {
+    document.getElementById('note8black').style.display="flex";
+    document.getElementById('note8blue').style.display="none";
+    document.getElementById('note8white').style.display="none";
+ }
+ 
+ function note8blue() {
+     document.getElementById('note8black').style.display="none";
+     document.getElementById('note8blue').style.display="flex";
+     document.getElementById('note8white').style.display="none";
+  }
+ 
+  function note8white() {
+     document.getElementById('note8black').style.display="none";
+     document.getElementById('note8blue').style.display="none";
+     document.getElementById('note8white').style.display="flex";
+  }
+  
+
+  
+  function note9progray() {
+    document.getElementById('note9progray').style.display="flex";
+    document.getElementById('note9progreen').style.display="none";
+    document.getElementById('note9prowhite').style.display="none";
+ }
+ 
+ function note9progreen() {
+     document.getElementById('note9progray').style.display="none";
+     document.getElementById('note9progreen').style.display="flex";
+     document.getElementById('note9prowhite').style.display="none";
+  }
+ 
+  function note9prowhite() {
+     document.getElementById('note9progray').style.display="none";
+     document.getElementById('note9progreen').style.display="none";
+     document.getElementById('note9prowhite').style.display="flex";
+  }
+
+
+
+
